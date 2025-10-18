375d7b8ff1f3731703244a0a4cf93df7ee32c527
y307ah <iaroslavkurpachenko@gmail.com>
2025-10-19 02:46:10 +0900

    функция для того чтобы убрать гигантский IF

--- commandConfig.cpp
+++ commandConfig.cpp
@@ -1,27 +1,25 @@
 #include "commandConfig.h"
 
 // Определяем массив команд с аргументами
-const int COMMAND_NEEDS_ARGUMENT[] = {
-    [CMD_PUSH] = 1,
-    [CMD_PUSHR] = 1,
-    [CMD_POPR] = 1,
-    [CMD_JMP] = 1,
-    [CMD_JB] = 1,
-    [CMD_JBE] = 1,
-    [CMD_JE] = 1,
-    [CMD_JNE] = 1,
-    [CMD_JA] = 1,
-    [CMD_JAE] = 1,
-    [CMD_CALL] = 1
-};
-
 int commandHasArgument(int command)
 {
-    if (command >= 0 && command < sizeof(COMMAND_NEEDS_ARGUMENT)/sizeof(COMMAND_NEEDS_ARGUMENT[0]))
+    switch (command)
     {
-        return COMMAND_NEEDS_ARGUMENT[command];
+        case CMD_PUSH:
+        case CMD_PUSHR:
+        case CMD_POPR:
+        case CMD_JMP:
+        case CMD_JB:
+        case CMD_JBE:
+        case CMD_JE:
+        case CMD_JNE:
+        case CMD_JA:
+        case CMD_JAE:
+        case CMD_CALL:
+            return 1;
+        default:
+            return 0;
     }
-    return 0;
 }
 
 int shouldIncrementIP(int command)
