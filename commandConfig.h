b96b556de8a73fb36f09b2bc183fbc017bb175e9
y307ah <iaroslavkurpachenko@gmail.com>
2025-10-19 02:46:38 +0900

    header для функции против IFа

--- commandConfig.h
+++ commandConfig.h
@@ -3,12 +3,7 @@
 
 #include "processorOperations.h"
 
-// Массив команд, которые требуют аргумент (1 - требует, 0 - нет)
-extern const int COMMAND_NEEDS_ARGUMENT[];
-
-// Функция для проверки нужен ли команде аргумент
 int commandHasArgument(int command);
-
 int shouldIncrementIP(int command);
 
 #endif
